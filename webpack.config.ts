5720c360b988a4b5812ad6786b40b71229af3934
Matt Luongo <mhluongo@gmail.com>
2022-07-14 16:08:05 -0400

    WIP

---
+++ webpack.config.ts
@@ -0,0 +1,26 @@
+import path from "path"
+
+export default {
+  mode: "development",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: "./dist",
+    port: 8000,
+  },
+}
